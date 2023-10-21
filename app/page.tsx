0c81cf33e76906f8a0d5b829104b017544d0a590
jcbyte <57547887+jcbyte@users.noreply.github.com>
2023-10-21 20:47:48 +0100

    Created grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,37 @@
 "use client";
 
+import { Box } from "@mui/material";
+
+import { PropsWithChildren } from "react";
+
+export const GridItem = (props: PropsWithChildren) => {
+	return (
+		<Box
+			sx={{
+				"background-color": "rgb(100, 200, 200)",
+				"aspect-ratio": "1",
+			}}
+		>
+			{props.children}
+		</Box>
+	);
+};
+
 export default function Home() {
-	return <>Hello world</>;
+	return (
+		<>
+			<Box
+				sx={{
+					display: "grid",
+					"grid-template-columns": "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr",
+					"grid-gap": "5px",
+					margin: "10px",
+				}}
+			>
+				{[...Array(11 * 11)].map((item, i) => {
+					return <GridItem>{i}</GridItem>;
+				})}
+			</Box>
+		</>
+	);
 }
