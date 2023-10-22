35a55967aef26b50b8d33b1c44f1ca8e3fa57ec3
jcbyte <57547887+jcbyte@users.noreply.github.com>
2023-10-22 22:26:34 +0100

    Created interfaces and started creating UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,113 @@
 "use client";
 
-import { Box } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
 
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 
-export const GridItem = (props: PropsWithChildren) => {
+import DirectionsBoatIcon from "@mui/icons-material/DirectionsBoat";
+import WavesIcon from "@mui/icons-material/Waves";
+
+interface CellData {
+	shot: boolean;
+	hit: boolean;
+	probability: number;
+}
+
+interface GameTile {
+	type: "game";
+	data: CellData;
+}
+
+interface LabelTile {
+	type: "label";
+	text: string;
+}
+
+type Tile = GameTile | LabelTile;
+
+const BOARD_SIZE = 10;
+
+export const GridItem = (props: PropsWithChildren<{ tile: Tile }>) => {
+	var content = <></>;
+	if (props.tile.type == "label") {
+		content = <>{props.tile.text}</>;
+	} else if (props.tile.type == "game") {
+		content = (
+			<>
+				<Box sx={{ display: "flex", justifyContent: "center" }}>
+					<IconButton size="small">
+						<DirectionsBoatIcon />
+					</IconButton>
+					<IconButton size="small">
+						<WavesIcon />
+					</IconButton>
+				</Box>
+			</>
+		);
+	}
+
 	return (
 		<Box
 			sx={{
-				"background-color": "rgb(100, 200, 200)",
-				"aspect-ratio": "1",
+				backgroundColor: "#aa88aa",
+				aspectRatio: "1",
 			}}
 		>
-			{props.children}
+			{content}
 		</Box>
 	);
 };
 
 export default function Home() {
+	const [board, setBoard] = useState<CellData[][]>(
+		[...Array(BOARD_SIZE)].map(() => {
+			return [...Array(BOARD_SIZE)].map(() => {
+				return { shot: false, hit: false, probability: 0 } as CellData;
+			});
+		})
+	);
+
 	return (
 		<>
-			<Box
-				sx={{
-					display: "grid",
-					"grid-template-columns": "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr",
-					"grid-gap": "5px",
-					margin: "10px",
-				}}
-			>
-				{[...Array(11 * 11)].map((item, i) => {
-					return <GridItem>{i}</GridItem>;
-				})}
+			<Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+				<Typography variant="h2" fontWeight="bold">
+					Battleship Predictor
+				</Typography>
+				<Box
+					sx={{
+						display: "grid",
+						gridTemplateColumns: `repeat(${BOARD_SIZE + 1}, 1fr)`,
+						gridGap: "1px",
+						margin: "10px",
+						width: "1000px",
+						padding: "0px",
+						backgroundColor: "#000",
+						borderRadius: "10px",
+						overflow: "hidden",
+					}}
+				>
+					{(
+						[
+							{ text: "", type: "label" } as LabelTile,
+							...[...Array<LabelTile>(BOARD_SIZE)].map((item, i) => {
+								return { text: "A", type: "label" } as LabelTile;
+							}),
+							,
+							...board
+								.map((row, i) => {
+									return [
+										{ text: "B", type: "label" } as LabelTile,
+										...row.map((item, i) => {
+											return { data: item, type: "game" } as GameTile;
+										}),
+									];
+								})
+								.flat(1),
+						] as Tile[]
+					).map((item, i) => {
+						return <GridItem key={i} tile={item} />;
+					})}
+				</Box>
 			</Box>
 		</>
 	);
