9d35bc5ad51106eae7c8ba91c7f4beb68bc2318b
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-08-14 22:52:17 +0100

    added keys to list elements

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -8,18 +8,18 @@ export default function Board({ board }: { board: CellData[][] }) {
 			<div className="rounded w-fit overflow-hidden">
 				<div className="flex flex-col gap-[2px]">
 					<div className="flex gap-[2px]">
-						<GridItem tile={{ type: "label", text: "" }} />
-						{COLUMN_IDENTIFIERS.map((label) => {
-							return <GridItem tile={{ type: "label", text: label }} />;
+						<GridItem key="col-0-row-0-blank" tile={{ type: "label", text: "" }} />
+						{COLUMN_IDENTIFIERS.map((label, i) => {
+							return <GridItem key={`col-${i}-label`} tile={{ type: "label", text: label }} />;
 						})}
 					</div>
 
 					{board.map((boardRow, rowNum) => {
 						return (
-							<div className="flex gap-[2px]">
-								<GridItem tile={{ type: "label", text: ROW_IDENTIFIERS[rowNum] }} />
-								{boardRow.map((cell: CellData) => {
-									return <GridItem tile={{ type: "game", ...cell }} />;
+							<div key={`row-${rowNum}-container`} className="flex gap-[2px]">
+								<GridItem key={`row-${rowNum}-label`} tile={{ type: "label", text: ROW_IDENTIFIERS[rowNum] }} />
+								{boardRow.map((cell: CellData, colNum) => {
+									return <GridItem key={`row-${rowNum}-col-${colNum}`} tile={{ type: "game", ...cell }} />;
 								})}
 							</div>
 						);
