42c26548eb828d1eeefbd9e2594ac6bc07e65787
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-08-20 19:10:22 +0100

    extracted heatmap incrementing to separate function

--- src/scripts/calculator.ts
+++ src/scripts/calculator.ts
@@ -2,11 +2,15 @@ import { BOARD_SIZE } from "../static";
 import { CellState, Ship } from "../types";
 import { factorial, getNewBoard } from "./utils";
 
+// Function to return how the number of hits on a ship affects the heatmap
 function hitsScaling(hits: number): number {
+	// If 0 then there is still a valid ship so there should be 1
 	if (hits == 0) {
 		return 1;
 	}
 
+	// This function is to increase the value for hits and adjacent hits
+	// If the hits calculations don't seem right this function should be edited
 	return factorial(hits * 2);
 }
 
@@ -48,6 +52,28 @@ function isShipValid(
 	return { valid: true, hits: hits };
 }
 
+// Increase all values on heatmap underneath a ship by a specified amount
+// This will only increase tiles marked as unknown as there are the only tiles affected by the heatmap
+// There is no error checking as we assume this has already been done
+function incrementShipHeatmap(
+	board: CellState[][],
+	heatmap: number[][],
+	value: number,
+	row: number,
+	col: number,
+	length: number,
+	direction: "horizontal" | "vertical"
+) {
+	for (let shipPart = 0; shipPart < length; shipPart++) {
+		// Calculate the tile position based on if the ship is horizontal or vertical
+		let tilePosition: { row: number; col: number } =
+			direction == "horizontal" ? { row: row, col: col + shipPart } : { row: row + shipPart, col: col };
+		if (board[tilePosition.row][tilePosition.col] == "unknown") {
+			heatmap[tilePosition.row][tilePosition.col] += value;
+		}
+	}
+}
+
 // Calculate the probability of a ship being at any location on the board
 export function calculateProbabilities(board: CellState[][], ships: Ship[]): number[][] {
 	// Get the list of all remaining un-sunk ships
@@ -73,21 +99,13 @@ export function calculateProbabilities(board: CellState[][], ships: Ship[]): num
 				// If the ship would fit horizontally then increment each of the possible tiles
 				let horizontal = isShipValid(board, row, col, ship.length, "horizontal");
 				if (horizontal.valid) {
-					for (let shipPart = 0; shipPart < ship.length; shipPart++) {
-						if (board[row][col + shipPart] == "unknown") {
-							heatmap[row][col + shipPart] += hitsScaling(horizontal.hits);
-						}
-					}
+					incrementShipHeatmap(board, heatmap, hitsScaling(horizontal.hits), row, col, ship.length, "horizontal");
 				}
 
 				// If the ship would fit vertically then increment each of the possible tiles
 				let vertical = isShipValid(board, row, col, ship.length, "vertical");
 				if (vertical.valid) {
-					for (let shipPart = 0; shipPart < ship.length; shipPart++) {
-						if (board[row + shipPart][col] == "unknown") {
-							heatmap[row + shipPart][col] += hitsScaling(vertical.hits);
-						}
-					}
+					incrementShipHeatmap(board, heatmap, hitsScaling(vertical.hits), row, col, ship.length, "vertical");
 				}
 			}
 		}
--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -9,6 +9,7 @@ export function getNewBoard<T>(initial: T): T[][] {
 	});
 }
 
+// Calculate factorial of a number via recursion
 export function factorial(n: number): number {
 	return n <= 1 ? 1 : n * factorial(n - 1);
 }
