a7e9c202a93c361e3b6bf4cd9ad14627236ba29d
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-08-20 18:08:34 +0100

    merged horizontal and vertical ship checking into one function

--- src/scripts/calculator.ts
+++ src/scripts/calculator.ts
@@ -2,38 +2,29 @@ import { BOARD_SIZE } from "../static";
 import { CellState, Ship } from "../types";
 import { getNewBoard } from "./utils";
 
-// ? Can probably split parts of this into separate functions to avoid code duplication
-// ? Should we do horizontal and vertical in there own loops so we don't need edge detection
-
 const INVALID_SHIP_STATES: CellState[] = ["miss", "sunk"];
 
-function isHorizontalShipValid(board: CellState[][], row: number, col: number, length: number): boolean {
+function isShipValid(
+	board: CellState[][],
+	row: number,
+	col: number,
+	length: number,
+	direction: "horizontal" | "vertical"
+): boolean {
 	// If ship goes off the edge of the board it is not valid
-	if (col + length > BOARD_SIZE) {
-		return false;
-	}
-
-	// If any tiles the ship is on would cause it to be invalid return
-	for (let shipPart = 0; shipPart < length; shipPart++) {
-		let shipPartState = board[row][col + shipPart];
-		if (INVALID_SHIP_STATES.includes(shipPartState)) {
+	if (direction == "horizontal") {
+		if (col + length > BOARD_SIZE) {
+			return false;
+		}
+	} else {
+		if (row + length > BOARD_SIZE) {
 			return false;
 		}
 	}
 
-	// If all tiles are valid then this ship placement is valid
-	return true;
-}
-
-function isVerticalShipValid(board: CellState[][], row: number, col: number, length: number): boolean {
-	// If ship goes off the edge of the board it is not valid
-	if (row + length > BOARD_SIZE) {
-		return false;
-	}
-
 	// If any tiles the ship is on would cause it to be invalid return
 	for (let shipPart = 0; shipPart < length; shipPart++) {
-		let shipPartState = board[row + shipPart][col];
+		let shipPartState = direction == "horizontal" ? board[row][col + shipPart] : board[row + shipPart][col];
 		if (INVALID_SHIP_STATES.includes(shipPartState)) {
 			return false;
 		}
@@ -67,14 +58,14 @@ export function calculateProbabilities(board: CellState[][], ships: Ship[]): num
 		for (let row = 0; row < BOARD_SIZE; row++) {
 			for (let col = 0; col < BOARD_SIZE; col++) {
 				// If the ship would fit horizontally then increment each of the possible tiles
-				if (isHorizontalShipValid(board, row, col, ship.length)) {
+				if (isShipValid(board, row, col, ship.length, "horizontal")) {
 					for (let shipPart = 0; shipPart < ship.length; shipPart++) {
 						heatmap[row][col + shipPart]++;
 					}
 				}
 
 				// If the ship would fit vertically then increment each of the possible tiles
-				if (isVerticalShipValid(board, row, col, ship.length)) {
+				if (isShipValid(board, row, col, ship.length, "vertical")) {
 					for (let shipPart = 0; shipPart < ship.length; shipPart++) {
 						heatmap[row + shipPart][col]++;
 					}
